396a3fcbd648ea2bb5693a0c9b1ff94c2dee74e9
andrewWong21 <awong21@stuy.edu>
2017-11-17 14:30:27 -0500

    started working on database

---
+++ utils/userOperations.py
@@ -0,0 +1,29 @@
+import sqlite3   #enable control of an sqlite database
+import hashlib   #allows for passwords to be encrypted
+
+
+
+def createTables():
+    f="data/dingbubble.db"
+
+    db = sqlite3.connect(f) #open if f exists, otherwise create
+    c = db.cursor() #facilitate db ops
+
+    command - ""
+    c.execute(command)
+    
+    db.commit()
+    db.close()
+
+def getHighscores(): #user
+    # returns user's highscores formatted as 
+    pass
+
+def addHighscore(): #user, subject
+    pass
+
+def findUser(): #user, password
+    pass
+
+def addUser(): #user, password
+    pass
