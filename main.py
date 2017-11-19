02e8945cea89d348b1606c333305da53c27f4772
Max Z <max.zlotskiy@gmail.com>
2017-11-19 14:51:03 -0500

    wrote basic outline of all routes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, session, redirect, url_for, request, flash
+import os
 
 app = Flask(__name__)
 
@@ -21,9 +22,90 @@ def login_logic():
 
 @app.route('/home')
 def user_page():
-    return render_template("user.html")
+    if not ('user' in session):
+        flash("Must be logged in to view home page")
+        return redirect(url_for("root_route"))
+    else:
+        return render_template("user.html")
+
+@app.route('/scores')
+def view_high_scores():
+    return "High scores template"
+
+@app.route('/select')
+def select_quiz():
+    if not ('user' in session):
+        flash("Must be logged in to take a quiz")
+        return redirect(url_for("root_route"))
+    else:
+        return render_template("select.html")
+
+#select.html should redirect here
+#this will set up cookies for the quiz then redirect to /play
+@app.route('/launch', methods=["POST"])
+def begin_quiz():
+    if not ('user' in session):
+        flash("Must be logged in to take a quiz")
+        return redirect(url_for("root_route"))
+    else:
+        #set up cookies for quiz
+        session["score"] = str(0)
+        session['record'] = 0 #replace with call to DB to get high score
+        session['genre'] = request.form.get("genre", "22") #geography by default
+        session['difficulty'] = request.form.get("difficulty", "easy") #easy by default
+        session['format'] = request.form.get("type", "multiple") #multiple choice by default
+        return redirect(url_for("take_quiz"))
+
+#Displays a question and when the user answers it redirects to /check with the answer choice ID
+@app.route('/play')
+def take_quiz():
+    if not ('user' in session):
+        flash("Must be logged in to take a quiz")
+        return redirect(url_for("root_route"))
+    elif not set(['score', 'record', 'genre', 'difficulty', 'format'].issubset(session)):
+        flash("You must select a quiz first")
+        return redirect(url_for("user_page"))
+    else:
+        """
+        1) get question using trivia api
+        2) render the template
+        """
+        return "This page displays one question"
+
+#selecting an answer to a question in /play sends you here
+#if you got it right, sends to /play to answer another
+@app.route('/check', methods=["POST"])
+def check_answer():
+    """
+    1) check if answer is correct by testing if the answer code is divisible by 7
+    2) if it is divisible by 7, then add to score and redirect to /play
+    3) if it is not, then redirect to /results
+    """
+    return "This route should not be accessed by users"
+
+#Getting a question wrong sends you here
+@app.route('/results')
+def score_report():
+    if not ('user' in session):
+        flash("Must be logged in to view this page")
+        return redirect(url_for("root_route"))
+    elif not set(['score', 'record', 'genre', 'difficulty', 'format'].issubset(session)):
+        flash("You must take a quiz to see the score")
+        return redirect(url_for("user_page"))
+    else:
+        score = int(session.get("score", "0"))
+        high = int(session.get("record", "0"))
+        #render a template telling the user their score
+        return "You got " + str(score) + " correct answers"
+
+#Log out route, redirects to home page when done
+@app.route('/logout')
+def logout():
+    if 'user' in session:
+        session.pop("user")
+    return redirect(url_for("root_route"))
 
 if __name__ == "__main__":
     app.debug = True
-    app.secret_key = "just you, me, and the rest of the world"
+    app.secret_key = os.urandom(32)
     app.run()
