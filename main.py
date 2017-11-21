d5c28a98025bf0f75e36c990a7b638e9202d2da0
Max Z <max.zlotskiy@gmail.com>
2017-11-21 14:45:10 -0500

    cleaned up code using loggedIn and takingQuiz

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, session, redirect, url_for, request, flash
 import os
 
+
 app = Flask(__name__)
+#Returns true if the session has necessary keys set when launching quiz
+def takingQuiz():
+    return set(['score', 'record', 'genre', 'difficulty', 'format']).issubset(session)
+#Returns true if the user is logged in by checking for that key
+def loggedIn():
+    return 'user' in session
+
 
 @app.route('/')
 def root_route():
@@ -22,30 +30,34 @@ def login_logic():
 
 @app.route('/home')
 def user_page():
-    if not ('user' in session):
+    if not loggedIn():
         flash("Must be logged in to view home page")
         return redirect(url_for("root_route"))
     else:
-        return render_template("user.html")
+        return render_template("user.html", username=session['user'])
 
 @app.route('/scores')
 def view_high_scores():
-    return "High scores template"
+    if not loggedIn():
+        flash("Must be logged in to view your scores")
+        return redirect(url_for("root_route"))
+    else:
+        return "High scores template"
 
 #This is where the user selects a quiz genre
 @app.route('/select')
 def select_quiz():
-    if not ('user' in session):
+    if not loggedIn():
         flash("Must be logged in to take a quiz")
         return redirect(url_for("root_route"))
     else:
-        return render_template("select.html")
+        return render_template("select.html", username=session['user'])
 
 #/select should redirect here, this is a functional route (no html)
 #this will set up cookies for the quiz then redirect to /play
 @app.route('/launch', methods=["POST"])
 def begin_quiz():
-    if not ('user' in session):
+    if not loggedIn():
         flash("Must be logged in to take a quiz")
         return redirect(url_for("root_route"))
     else:
@@ -60,10 +72,10 @@ def begin_quiz():
 #Displays a question and when the user answers it redirects to /check with the answer choice ID
 @app.route('/play')
 def take_quiz():
-    if not ('user' in session):
+    if not loggedIn():
         flash("Must be logged in to take a quiz")
         return redirect(url_for("root_route"))
-    elif not set(['score', 'record', 'genre', 'difficulty', 'format'].issubset(session)):
+    elif not takingQuiz():
         flash("You must select a quiz first")
         return redirect(url_for("user_page"))
     else:
@@ -77,20 +89,27 @@ def take_quiz():
 #if you got it right, sends to /play to answer another
 @app.route('/check', methods=["POST"])
 def check_answer():
-    """
-    1) check if answer is correct by testing if the answer code is divisible by 7
-    2) if it is divisible by 7, then add to score and redirect to /play
-    3) if it is not, then redirect to /results
-    """
-    return "This route should not be accessed by users"
+    if not loggedIn():
+        flash("Must be logged in to take a quiz")
+        return redirect(url_for("root_route"))
+    elif not takingQuiz():
+        flash("You must select a quiz first")
+        return redirect(url_for("user_page"))
+    else:
+        """
+        1) check if answer is correct by testing if the answer code is divisible by 7
+        2) if it is divisible by 7, then add to score and redirect to /play
+        3) if it is not, then redirect to /results
+        """
+        return "This route should not be accessed by users"
 
 #Getting a question wrong sends you here
 @app.route('/results')
 def score_report():
-    if not ('user' in session):
+    if not loggedIn():
         flash("Must be logged in to view this page")
         return redirect(url_for("root_route"))
-    elif not set(['score', 'record', 'genre', 'difficulty', 'format'].issubset(session)):
+    elif not takingQuiz():
         flash("You must take a quiz to see the score")
         return redirect(url_for("user_page"))
     else:
@@ -106,15 +125,8 @@ def logout():
         session.pop("user")
     return redirect(url_for("root_route"))
 
+
 if __name__ == "__main__":
     app.debug = True
     app.secret_key = os.urandom(32)
     app.run()
-
-#Returns true if the session has necessary keys set when launching quiz
-def takingQuiz():
-    return set(['score', 'record', 'genre', 'difficulty', 'format']).issubset(session)
-
-#Returns true if the user is logged in by checking for that key
-def loggedIn():
-    return 'user' in session
