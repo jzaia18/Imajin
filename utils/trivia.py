b1555cb15b31203fbe1f0c2f4965c9cd5843a6df
imahdi2000 <imahdi@stuy.edu>
2017-11-26 18:24:31 -0500

    trivia api

--- utils/trivia.py
+++ utils/trivia.py
@@ -1,2 +1,32 @@
 import urllib2
 import json
+
+
+def gimmie(genre, difficulty, type):
+        num = 1
+    #    amount = "amount=1&"
+    #    category = "category" + "=" + request.form["genre"] + "&"
+    #    difficulty = "difficulty" + "=" + request.form["difficulty"] + "&"
+    #    Type = "type" + "=" + request.form["type"]
+    #    layout = request.form["type"]
+
+        #snippet = amount + category + difficulty + Type
+        snippet = 'amount=1&category=22&difficulty=easy&type=multiple'
+
+        u = urllib2.urlopen("https://opentdb.com/api.php?" + snippet)
+        data_string = u.read()
+        dic = json.loads(data_string)
+        questions = []
+        answers = []
+        incorrect = []
+        if dic['response_code'] == 0:
+            results = dic["results"]
+            for i in range(num):
+                questions.append(results[i]["question"])
+                answers.append(results[i]["correct_answer"])
+                incorrect.append(results[i]['incorrect_answers'])
+            print questions + answers + incorrect
+        else:
+            print "Sorry, we don't have enough of those questions. Try again!"
+
+gimmie(1,1,1)
