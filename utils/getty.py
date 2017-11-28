45fde0f5ba9fbb3a046724ef2cd7d9926ab75814
Max Z <max.zlotskiy@gmail.com>
2017-11-27 21:13:07 -0500

    filters out unecessary words in search phrase

--- utils/getty.py
+++ utils/getty.py
@@ -16,15 +16,36 @@ def test():
     #print json
     print len(json)
 
-def search(phrase):
+def list_urls(phrase):
     url = "https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=" + phrase
+    print url
     opener = urllib2.build_opener()
     opener.addheaders = [('Api-Key', the_key)]
-    response = opener.open(url)
-    json_string = response.read()
-    dictionary = json.loads(json_string)
-    list_of_urls = []
-    if dictionary["result_count"] > 0:
-        for image in dictionary["images"]:
-            list_of_urls.append(image["display_sizes"][0]["uri"])
-    return list_of_urls
+    try:
+        response = opener.open(url)
+        json_string = response.read()
+        dictionary = json.loads(json_string)
+        list_of_urls = [""]
+        if dictionary["result_count"] > 0:
+            for image in dictionary["images"]:
+                list_of_urls.append(image["display_sizes"][0]["uri"])
+        return list_of_urls
+    except:
+        print "error"
+        return [""]
+
+def search(phrase):
+    #prepositions and articles
+    fillers = "the a an about above across after against along alongside amid among are around as at before behind below between but by did do does down during featured features following for from got had has have how in inside into is less like long longest many more much near never no not of off on onto opposite out outside over short shortest since than through to toward towards under underneath unlike until up upon versus via was were what when which why within without you".split(" ")
+    #so that every word has a space in front of it
+    phrase = " " + phrase.lower() + " "
+    #get rid of punctuation
+    phrase = phrase.replace(",", "").replace("?", "").replace(".", "").replace("'", "").replace('"', "")
+    #get rid of all fillers
+    for word in fillers:
+        phrase = phrase.replace(" "+word+" ", " ")
+    #get rid of extra whitespace
+    phrase = phrase.strip()
+    #limit phrase
+    phrase = "+".join(phrase.split(" ")[:3])
+    return list_urls(phrase)
